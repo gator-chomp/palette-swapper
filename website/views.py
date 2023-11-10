601f5976801e1b1d39e4bb4a9a391b10e3cb148c
Gong Bing Wong <gongbingwong@gmail.com>
2023-11-10 02:40:33 -0500

    (Task 1) Clean up code in files
    •	Delete everything mentioning “Note” from views.py, models.py.
    •	Delete “not accessed” “from” statements in views.py, models.py, and __init__.py.
    •	Removed create_database function.

--- website/views.py
+++ website/views.py
@@ -1,26 +1,7 @@
-from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_user, login_required, logout_user, current_user
-from .models import Note
-from . import db
-import json
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
-
-@views.route('/', methods=['GET','POST'])
-@login_required
-def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        
-        if len('note') < 1:
-            flash('empty note not allowed', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!!', category='success')
-    return render_template("home.html", user=current_user)
-
 
 @views.route('/colorBlindPage')
 @login_required
