315820534413979dd1d5431112e466bbe42cb22d
Gong Bing Wong <gongbingwong@gmail.com>
2023-11-28 00:52:58 -0500

    Comment out print debugging statements

--- website/views.py
+++ website/views.py
@@ -7,17 +7,17 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    print(current_user.logoutPage)
+    # print(current_user.logoutPage)
     current_user.logoutPage = request.url
-    print(current_user.logoutPage)
+    # print(current_user.logoutPage)
     db.session.commit()
     return render_template("home.html", user=current_user)
 
 @views.route('/colorBlindPage')
 @login_required
 def colorBlind():
-    print(current_user.logoutPage)
+    # print(current_user.logoutPage)
     current_user.logoutPage = request.url
-    print(current_user.logoutPage)
+    # print(current_user.logoutPage)
     db.session.commit()
     return render_template("colorBlind.html", user=current_user)
